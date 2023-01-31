4a0727a4975d7e806bb0df7ae615c5c913fd653e
AnanyaDoshi728 <wolfbay778@gmail.com>
2023-01-31 17:15:36 +0530

    linkedin page changed headings and gifs responsive

--- src/Pages/ExcelsiorPage/ExcelsiorBanner/ExcelsiorBanner.js
+++ src/Pages/ExcelsiorPage/ExcelsiorBanner/ExcelsiorBanner.js
@@ -4,7 +4,7 @@ import "../../../Global.css"
 
 const ExcelsiorBanner = () => {
   return (
-    <div className=" flex items-center mt-36 px-20 gap-5">
+    <div className=" flex items-center mt-36 px-20 gap-5 podcast-heading-div">
       <div className="flex-1">
         <h1 className=" lg:text-[9rem] md:text-[5rem] text-[2rem] text-[#FE420B] standard-heading-line-heights" style={{fontWeight: "700"}}>
           The <br />
@@ -13,7 +13,7 @@ const ExcelsiorBanner = () => {
         </h1>
       </div>
       <div>
-        <img className="rounded-3xl " src={ohmygod} alt="" />
+        <img className="rounded-3xl" id="podcast-image"  src={ohmygod} alt="" />
       </div>
     </div>
   );
